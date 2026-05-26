4f1d6c10873f98c08e8fd2a6eab294b86477e450
ryan <ryandsferrer@gmail.com>
2026-05-26 10:05:41 -0300

    added new config in back

--- BackEnd/App.ts
+++ BackEnd/App.ts
@@ -6,7 +6,7 @@ import cors from '@fastify/cors'
 
 import authPlugin from "./src/auth/middleware.js";
 
-import AllBooks from "./src/routes/routeUser/getBooks.js"
+import getBooks from "./src/routes/routeUser/getBooks.js"
 import decriptionFromBook from "./src/routes/routerlibrary/getDescription.js"
 import routerUserCreate from "./src/routes/routeUser/createUser.js"
 import routeDataBookGerenc from "./src/routes/routerlibrary/startGetBookInPartners.js"
@@ -31,7 +31,7 @@ const main = async()=>{
     await ServerMain.register(authPlugin);
 
     //rotas
-    ServerMain.route(AllBooks)
+    ServerMain.route(getBooks)
     ServerMain.route(routerUserCreate)
     ServerMain.route(routeDataBookGerenc)
     ServerMain.route(decriptionFromBook)
